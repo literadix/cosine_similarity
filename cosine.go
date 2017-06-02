d4ec48738df80cf206326d01be772b70d52675e8
Maciej A. Bednarz <literadix@users.noreply.github.com>
2017-06-02 16:01:24 +0200

    math.pov is slower by factor 5 than simple multiplication in go

--- cosine.go
+++ cosine.go
@@ -6,32 +6,41 @@ import (
 )
 
 func Cosine(a []float64, b []float64) (cosine float64, err error) {
+
 	count := 0
-	length_a := len(a)
-	length_b := len(b)
-	if length_a > length_b {
-		count = length_a
+	lengthA := len(a)
+	lengthB := len(b)
+
+	if lengthA > lengthB {
+		count = lengthA
 	} else {
-		count = length_b
+		count = lengthB
 	}
+
 	sumA := 0.0
 	s1 := 0.0
 	s2 := 0.0
+
 	for k := 0; k < count; k++ {
-		if k >= length_a {
-			s2 += math.Pow(b[k], 2)
+
+		if k >= lengthA {
+			s2 += b[k] * b[k]
 			continue
 		}
-		if k >= length_b {
-			s1 += math.Pow(a[k], 2)
+
+		if k >= lengthB {
+			s1 += a[k] * a[k]
 			continue
 		}
+
 		sumA += a[k] * b[k]
-		s1 += math.Pow(a[k], 2)
-		s2 += math.Pow(b[k], 2)
+		s1 += a[k] * a[k]
+		s2 += b[k] * b[k]
 	}
+
 	if s1 == 0 || s2 == 0 {
 		return 0.0, errors.New("Vectors should not be null (all zeros)")
 	}
+
 	return sumA / (math.Sqrt(s1) * math.Sqrt(s2)), nil
 }
